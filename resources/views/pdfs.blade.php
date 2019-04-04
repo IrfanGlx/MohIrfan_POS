3f6bd0708e13df4a506ad53ffb02cecf2e26b5ce
unknown <moh.i.sudrajat44@gmail.com>
2019-04-04 09:27:51 +0700

    test

---
+++ resources/views/pdfs.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+	table, td, th {  
+		border: 1px solid #ddd;
+		text-align: left;
+	}
+
+	table {
+		border-collapse: collapse;
+		width: 100%;
+	}
+
+	th, td {
+		padding: 15px;
+	}
+</style>
+<title></title>
+</head>
+<body>
+	<center>
+		<i>
+			<h2>POS SMKN 10 Jakarta</h2>
+			<h4 style="margin-top: -25px;">Jl. Mayjend Sutoyo, Cawang, KramatJati</h4>
+			<h4 style="margin-top: -20px;">Rt/Rw 02/09, Jakarta Timur</h4>
+		</i>
+	</center>
+	<div style="margin-left: 5px;">
+		<h4>No Telp: (021) 8091773</h4>
+		<h4 style="margin-top: -20px;">Kode Pos: 13630</h4>
+		<h4 style="margin-top: -20px;">Tahun 1997 SMEA Negeri 6 Jakarta berubah namanya menjadi SMK Negeri 10 Jakarta pada  tanggal 7 Maret 1997  berdasarkan Keputusan Menteri Pendidikan No.036/O/1997.</h4>
+	</div>
+	<table>
+		<thead>
+			<tr>
+				<th class="sortable-column sort-asc text-center">Nama</th>
+				<th class="sortable-column sort-asc text-center">Kode Transaksi</th>
+				<th class="sortable-column sort-asc text-center">Qty</th>
+				<th class="sortable-column sort-asc text-center">Harga</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($pos as $key)
+			<tr>
+				<td>{{$key->nama}}</td>
+				<td>{{$key->kodetransaksi}}</td>
+				<td>{{$key->jumlah}} {{$key->unit}}</td>
+				<td>Rp. {{$key->harga}}</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</body>
+</html>
